4b92545d33fcf700155ebf220bb1998e99e26fe1
Bart Thate <bthate@dds.nl>
2019-10-25 14:51:48 +0200

    remove nr and aangifte commands

--- obot/cmd/cmds.py
+++ obot/cmd/cmds.py
@@ -6,8 +6,3 @@ def cmds(event):
     """ show commands. """
     event.reply("|".join(sorted(k.cmds)))
 
-def nr(event):
-    event.reply("EM_T04_OTP-CR-117_19")
-    
-def aangifte(event):
-    event.reply("Aangifte van genocide misdrijven door toediening van gif (impotent maken, martelen, doden): dit nummer is de aangifte dat de koning weet dat het gif is en dus genocide. Aangifte nr. EM_T04_OTP-CR-117_19 - https://genoclaim.readthedocs.io #ggz #gifpil")
